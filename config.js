14b12ea300bacfaf162559c92be7b02df6569370
synyx1337 <102131781+synyx1337@users.noreply.github.com>
2022-04-23 10:35:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
+        turn_on: "点亮",
         play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        bannar_coming: "彩旗",
+        balloons_flying: "Surprise!",
+        cake_fadein: "蛋糕",
+        light_candle: "蜡烛",
+        wish_message: "生日快乐MY DEAR",
         story: "A MESSAGE FOR YOU",
     }
 };
